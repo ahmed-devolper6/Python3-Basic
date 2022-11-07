e30e010260635ac0c6f86d2625c7800e79934e7f
jamkia01010 <ahmedjamal100@gmail.com>
2022-11-07 15:45:19 +0200

    add Operators

---
+++ Python-Operators/Operators.py
@@ -0,0 +1,53 @@
+'''
+    Operators are used to perform operations on variables and values.
+    In the example below, we use the + operator to add together two values
+'''
+
+'''
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+and many of them
+'''
+x = 10
+y = 150 
+'''
+Arithmetic Operators:
+
+    Arithmetic operators are used with numeric values to perform common mathematical operations:
+'''
+
+print(x+y)
+# 160 int number
+print(x/y)
+#0.06666666666666667 float number
+print(x-y)
+#-140 
+print(x*y)
+#1500
+print(x%y)
+#10
+print(x**y)
+#1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+print(x//y)
+#0
+
+
+
+'''
+Python Assignment Operators
+    Assignment operators are used to assign values to variables:
+'''
+x = 5
+
+print(x)
+#5
+w = 5
+w +=3
+print(w)
+#8
+w -=1
+print(w)
+# 7
+ 
