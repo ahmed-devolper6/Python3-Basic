35352a5cc35f08c73ced2c38a4e043c9873080e0
jamkia01010 <ahmedjamal100@gmail.com>
2022-11-07 15:34:48 +0200

    Python Variables

---
+++ Python Variables/Variables.py
@@ -0,0 +1,13 @@
+'''
+    python is dyminc type to declaring i variable
+    you well not need to declaring the data type of variable
+exmple 1:
+'''
+x = 5
+'''
+5 is int python know that 
+'''
+print(x)
+'''
+output is 5
+'''
