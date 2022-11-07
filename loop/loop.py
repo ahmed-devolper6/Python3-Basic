cdac05400086da2bba747de414e90bf4b93900a2
jamkia01010 <ahmedjamal100@gmail.com>
2022-11-07 15:53:19 +0200

    add the loops

---
+++ loop/loop.py
@@ -0,0 +1,22 @@
+'''
+Python has two primitive loop commands:
+
+while loops
+for loops
+'''
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
+#print from 1 to 5
+
+w = 5 
+for x in range(w+1):
+    print(x)
+
+
+#print from 1 to 5
